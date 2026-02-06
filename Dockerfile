5470eb05ac0cf8db62242380a9039dec3ba3c52a
guihocosta <guiihocosta@gmail.com>
2026-02-06 15:26:05 -0300

    Faxina: removendo pycache e atualizando gitignore

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-# https://airflow.apache.org/docs/docker-stack/build.html
-FROM apache/airflow:3.0.1
-COPY requirements.txt /
-RUN pip install --no-cache-dir -r /requirements.txt
--- dags/Importador/util/date_utils.py
+++
@@ -1,9 +0,0 @@
-from dateutil import parser
-from datetime import timezone
-
-class DateUtils:
-    @staticmethod
-    def format_date_to_iso(date_str):
-        dt_obj = parser.parse(date_str)
-        dt_utc = dt_obj.astimezone(timezone.utc)
-        return dt_utc.isoformat(timespec='milliseconds').replace('+00:00', 'Z')
